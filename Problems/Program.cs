e11db101ea4cb00bd3c3fca975511d851f9e2117
Tandza Khoza <G17K7453@ict.ru.ac.za>
2020-02-20 17:00:11 +0200

    modified cs file

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -5,8 +5,36 @@ namespace Problems
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {    Programs p=new Programs();
+            Console.WriteLine(p.prefix("Hello World!"));
+        
         }
+        
+       public class Programs
+        {
+
+             public string prefix(string s)
+             {
+                 int length= s.Length;
+                 string[] words =s.Split(' ');
+                 int numwords =0;
+                 foreach(string ss in words)
+                 {
+                      numwords++;
+                 }
+
+                 return length+","+numwords+":"+s;
+
+             }
+
+
+        }
+
+
+
+
+
+
+
     }
 }
